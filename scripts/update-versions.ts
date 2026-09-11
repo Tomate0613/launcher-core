94e3042c9a19e362773deb57f5b93a17daa38656
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-09-11 21:34:05 +0200

    update update-versions script

--- scripts/update-versions.ts
+++ scripts/update-versions.ts
@@ -1,10 +1,16 @@
-import { vanilla } from "tomate-loaders";
 import fs from "node:fs";
-import { ensureDirectoryExists } from "../src/utils";
+import { ensureDirectoryExists } from "../src/utils.ts";
+import type { VersionManifest } from "../src";
 
 async function run() {
-  const versions = await vanilla.listSupportedGameVersions();
-  const ids = versions.map((version) => version.version).reverse();
+  const manifest = (await (
+    await fetch(
+      "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json",
+    )
+  ).json()) as VersionManifest;
+
+  const versions = manifest.versions.map((version) => version.id);
+  const ids = versions.reverse();
 
   ensureDirectoryExists("src/data");
   fs.writeFileSync(
